d659f997714fc73d54dd234edd7cc7b8df2e45c7
DecheanSherpa <sdechean20@tbc.edu.np>
2023-03-19 15:56:04 +0545

    Practice work

--- app/src/main/java/com/example/tiptime/MainActivity.java
+++ app/src/main/java/com/example/tiptime/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tiptime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private double tipAmount;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         choices = findViewById(R.id.radioGroup);
         aSwitch = findViewById(R.id.switch1);
         textView = findViewById(R.id.tipAmount);
+
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,8 +63,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 textView.setText(""+tipAmount);
 
+
             }
         });
+
+
+
+
 
     }
 }
